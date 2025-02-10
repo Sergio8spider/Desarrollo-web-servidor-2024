1102752d927a66b18d10808ee6cb5c0e50972033
Sergio <sergioam3005@gmail.com>
2025-02-10 14:07:21 +0100

    seguimos con laravel

--- 08_laravel/ejemplo/routes/web.php
+++ 08_laravel/ejemplo/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CocheController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/coches', function () {
+    return view('hello');
+});
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return view('hello');
 });
